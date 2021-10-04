18978a6f4b6f79c61254bafa7fde73e715de60ee
Antoni Sawicki <tenox@google.com>
2021-10-04 01:19:20 -0700

    update build script

--- build.cmd
+++ build.cmd
@@ -1,9 +1,10 @@
 set APP=snoopy
+set VCVARS="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat"
 
 set INCLUDE=
 set LIB=
 set LIBPATH=
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+call %VCVARS% amd64
 echo on
 del %APP%-x64.exe 
 cl /Fe%APP%-x64.exe %APP%.c 
@@ -11,7 +12,7 @@ cl /Fe%APP%-x64.exe %APP%.c
 set INCLUDE=
 set LIB=
 set LIBPATH=
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64_x86
+call %VCVARS% amd64_x86
 echo on
 del %APP%-x86.exe 
 cl /Fe%APP%-x86.exe %APP%.c 
@@ -19,7 +20,7 @@ cl /Fe%APP%-x86.exe %APP%.c
 set INCLUDE=
 set LIB=
 set LIBPATH=
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64_arm
+call %VCVARS% amd64_arm
 echo on
 del %APP%-arm.exe 
 cl /D_ARM_WINAPI_PARTITION_DESKTOP_SDK_AVAILABLE /Fe%APP%-arm.exe %APP%.c 
